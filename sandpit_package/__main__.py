7aef79e3ef94be7c25eb86f85f1e42a38001e518
Matthew L Davis <mld.0@protonmail.com>
2022-06-25 00:54:28 +1000

    delete whitespace

--- sandpit_package/__main__.py
+++ sandpit_package/__main__.py
@@ -3,7 +3,7 @@
 #   {{{2
 #   Ongoings:
 #   {{{
-#   Ongoing: 2022-06-23T23:57:13AEST how __main__.py should handle imports from within package (runnable with/without installing?) (also needed for tests) [...] 
+#   Ongoing: 2022-06-23T23:57:13AEST how __main__.py should handle imports from within package (runnable with/without installing?) (also needed for tests) [...]
 #   Ongoing: 2022-06-24T00:09:24AEST should package be runnable without 'installing' (either adding to python-path, or using 'pip install') anyway? (recall: don't try and <trick/hack> 'import')
 #   Ongoing: 2022-06-24T00:11:32AEST does lack of '__main__.py' denote a python package 'not executable' (can we not set a run-package command in some config file?)
 #   }}}
@@ -16,7 +16,7 @@
 def main():
     #   run package
     return 0
-    
+
 
 if __name__ == '__main__':
     main()
